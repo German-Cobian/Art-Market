646ec095a54584a633e77e9ccf76fbb41cd198ef
German Renato Cobian <germancobian@hotmail.com>
2024-03-21 14:50:36 -0700

    Create functionality for the javascript artworks controller + Teat that it affects local storage

--- app/javascript/controllers/artworks_controller.js
+++ app/javascript/controllers/artworks_controller.js
@@ -7,6 +7,35 @@ export default class extends Controller {
 
   addToCart() {
     console.log("artwork:", this.artworkValue) 
-  }
+  
+    console.log("artwork", this.artworkValue)
+    const cart = localStorage.getItem("cart")
+
+    if (cart) {
+      const cartArray = JSON.parse(cart)
+      const foundIndex = cartArray.findIndex(item => item.id === this.atworkValue.id)
+      if (foundIndex >= 0) {
+        cartArray[foundIndex].quantity = parseInt(cartArray[foundIndex].quantity) + 1
+      } else {
+        const Array = []
+        cartArray.push ({
+          id: this.artworkValue.id,
+          title: this.artworkValue.title,
+          price: this.artworkValue.price,
+          quantity: 1
+        })
+      }
+      localStorage.setItem("cart", JSON.stringify(cartArray))
+    } else {
+      const cartArray = []
+      cartArray.push({
+        id: this.artworkValue.id,
+        title: this.artworkValue.title,
+        price: this.artworkValue.price,
+        quantity: 1
+      })
+      localStorage.setItem("cart", JSON.stringify(cartArray))
+    }
+  };
 
 }
